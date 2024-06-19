168001c56faf7bf1ad03ec8fbf4c1a1278136d84
Parth Pandya <211290107020p@gmit.edu.in>
2024-06-19 23:24:39 +0530

    arrays

---
+++ C++/Opps/arrays.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  std::cout << "Hello World!\n";
+  // int arr[4];
+  // cin >> arr[0] >> arr[1] >> arr[2]  >> arr[3] ;
+  // cout << arr[0] << arr[1] << arr[2] << arr[3] << endl;
+
+  // arr[3] = 16; also can define the value;;
+
+  //2d Array;
+//   int arr [2][3];
+//   arr[1][1] = 78; //if we not specified then it take garbage value;
+// cout << arr[1][1];
+
+  //string;
+
+  string s = "Parth"; 
+  int len = s.size();
+  // cout<< s[2];
+  cout<< s[len - 1] << endl; //h.
+
+  s[len - 1] = 'Z';
+  cout<< s[len - 1]; //z
+
+  
+}
