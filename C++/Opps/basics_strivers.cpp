295c54cd23b537f954c27380f79cbc015473133b
Parth Pandya <211290107020p@gmit.edu.in>
2024-06-19 23:21:53 +0530

    basic of cpp

---
+++ C++/Opps/basics_strivers.cpp
@@ -0,0 +1,27 @@
+// #include <iostream>
+// #include <math.h>
+// #include <string>
+#include <bits/stdc++.h> //it includes all the library;
+
+using namespace std;
+int main() {
+  // std::cout << "Hello World!" << endl << "hey bro" << endl ;
+
+  // int x;
+  // cin >> x;
+  // cout << "the value of X : " << x;
+
+  // string str1;
+  // string str2;     //it take only one string means before space onlyone part..
+  // cin >> str1 >> str2;
+  // cout << str1 << " " << str2;
+
+  // so if we want to do more than one string then we use 
+  // string str;
+  // getline(cin,str);
+
+  char ch = 'a';
+  cout << ch;
+  
+  return 0;
+}
